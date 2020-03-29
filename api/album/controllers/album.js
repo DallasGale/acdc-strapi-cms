138827d8e40d733b47ba9479c5f8deb4063b0cf2
Dallas Gale <dallasgale.digital@gmail.com>
2020-03-30 09:16:02 +1100

    ?

--- api/album/controllers/album.js
+++ api/album/controllers/album.js
@@ -279,28 +279,27 @@ module.exports = {
               if (singles[i].albumSingle) {
                 singles[i].details = singles[i].albumSingle;
                 delete singles[i].albumSingle;
-              }
 
-              // if (singles[i].details[0].id) {
-              //   delete singles[i].details[0].id;
-              // }
+                // if (singles[i].details[0].id) {
+                delete singles[i].details[0].id;
+                // }
 
-              // if (singles[i].single) {
-              //   delete singles[i].single;
-              // }
+                // if (singles[i].single) {
+                delete singles[i].single;
+                // }
 
-              // if (singles[i].albums) {
-              //   delete singles[i].albums;
-              // }
+                // if (singles[i].albums) {
+                delete singles[i].albums;
+                // }
 
-              // if (singles[i].isLiveRecording) {
-              //   delete singles[i].isLiveRecording;
-              // }
+                // if (singles[i].isLiveRecording) {
+                delete singles[i].isLiveRecording;
+                // }
 
-              // if (singles[i].liveAlbum) {
-              //   delete singles[i].liveAlbum;
-              // }
-              if (singles[i].details[0]) {
+                // if (singles[i].liveAlbum) {
+                delete singles[i].liveAlbum;
+                // }
+                // if (singles[i].details[0]) {
                 // singles[i].industryCharts =
                 //   singles[i].details[0].industryCharts;
                 // delete singles[i].details[0].industryCharts;
