b8bb8d19aca3091af6656e4191087049843adc1e
Dallas Gale <dallasgale.digital@gmail.com>
2020-03-29 23:07:03 +1100

    ?

--- api/album/controllers/album.js
+++ api/album/controllers/album.js
@@ -276,30 +276,30 @@ module.exports = {
               singles[i] = allSongs[c];
               delete singles[i].id;
 
-              if (singles[i].albumSingle) {
-                singles[i].details = singles[i].albumSingle;
-                delete singles[i].albumSingle;
-              }
+              // if (singles[i].albumSingle) {
+              //   singles[i].details = singles[i].albumSingle;
+              //   delete singles[i].albumSingle;
+              // }
 
-              if (singles[i].details[0].id) {
-                delete singles[i].details[0].id;
-              }
+              // if (singles[i].details[0].id) {
+              //   delete singles[i].details[0].id;
+              // }
 
-              if (singles[i].single) {
-                delete singles[i].single;
-              }
+              // if (singles[i].single) {
+              //   delete singles[i].single;
+              // }
 
-              if (singles[i].albums) {
-                delete singles[i].albums;
-              }
+              // if (singles[i].albums) {
+              //   delete singles[i].albums;
+              // }
 
-              if (singles[i].isLiveRecording) {
-                delete singles[i].isLiveRecording;
-              }
+              // if (singles[i].isLiveRecording) {
+              //   delete singles[i].isLiveRecording;
+              // }
 
-              if (singles[i].liveAlbum) {
-                delete singles[i].liveAlbum;
-              }
+              // if (singles[i].liveAlbum) {
+              //   delete singles[i].liveAlbum;
+              // }
               // if (singles[i].details[0]) {
               //   singles[i].industryCharts =
               //     singles[i].details[0].industryCharts;
