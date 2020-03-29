831475b8790ddafd11143b172e24e08c175523b8
Dallas Gale <dallasgale.digital@gmail.com>
2020-03-29 23:10:54 +1100

    ?

--- api/album/controllers/album.js
+++ api/album/controllers/album.js
@@ -276,10 +276,10 @@ module.exports = {
               singles[i] = allSongs[c];
               delete singles[i].id;
 
-              // if (singles[i].albumSingle) {
-              //   singles[i].details = singles[i].albumSingle;
-              //   delete singles[i].albumSingle;
-              // }
+              if (singles[i].albumSingle) {
+                singles[i].details = singles[i].albumSingle;
+                delete singles[i].albumSingle;
+              }
 
               // if (singles[i].details[0].id) {
               //   delete singles[i].details[0].id;
