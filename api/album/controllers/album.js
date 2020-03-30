4f91789f3cda5ba167e00587c11845b030f2a225
Dallas Gale <dallasgale.digital@gmail.com>
2020-03-30 20:33:24 +1100

    ?

--- api/album/controllers/album.js
+++ api/album/controllers/album.js
@@ -47,6 +47,7 @@ module.exports = {
   async find(ctx) {
     let album_entities;
     let song_entities;
+    const allSongs = song_entities.map(song => song);
 
     if (ctx.query._q) {
       console.log("searching...");
@@ -269,7 +270,6 @@ module.exports = {
       // * Singles...
       if (album_entities[i].singles.length) {
         singles = album_entities[i].singles[0].songs;
-        const allSongs = song_entities.map(song => song);
         singles.map((a, i) => {
           allSongs.map((b, c) => {
             if (b.id === a.id) {
@@ -280,22 +280,18 @@ module.exports = {
                 typeof singles[i].albumSingle !== "undefined" ||
                 singles[i].albumSingle !== null
               ) {
-                singles[i].monthReleasedUK =
-                  singles[i].details[0].MonthReleasedUK;
-                delete singles[i].details[0].MonthReleasedUK;
-
+                // singles[i].monthReleasedUK =
+                //   singles[i].details[0].MonthReleasedUK;
+                // delete singles[i].details[0].MonthReleasedUK;
                 // singles[i].monthReleasedUS =
                 //   singles[i].details[0].MonthReleasedUS;
-                // delete singles[i].details[0].MonthReleasedUS;
-
+                // delete singles[i].details[-].MonthReleasedUS;
                 // singles[i].releasedYearUK =
                 //   singles[i].details[0].releasedYearUK;
                 // delete singles[i].details[0].releasedYearUK;
-
                 // singles[i].releasedYearUS =
                 //   singles[i].details[0].releasedYearUS;
                 // delete singles[i].details[0].releasedYearUS;
-
                 // singles[i].releasedAus = singles[i].details[0].releasedAus;
                 // delete singles[i].details[0].releasedAus;
               }
